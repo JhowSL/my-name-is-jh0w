2ccbbdc90394c0f2aff19a86cefdcbd4b5a7ff65
Jh0w <jon.1lima@hotmail.com>
2024-07-08 16:05:13 -0300

    feat: database, collections and schemas for Contact Form, Skills, Certificates and Projects

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,1 +1,33 @@
-export * from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  try {
+    const newContact = await prisma.contactForm.create({
+      data: {
+        name: 'Empresa1',
+        contactEmail: 'wvL2c@example.com',
+        message: 'teste',
+      }
+    })
+    console.log("Novo formulário de contato criado:", newContact);
+
+    const allContacts = await prisma.contactForm.findMany();
+    console.log("Todos os formulários de contato:", allContacts);
+  } catch (error) {
+    console.error("Erro ao criar formulário de contato:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+
+main()
+  .catch(async (e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
