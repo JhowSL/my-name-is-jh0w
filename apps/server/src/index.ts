4f282eaefd9e23394e240f56fab8ff0b73d430c6
Jh0w <jon.1lima@hotmail.com>
2024-07-24 01:02:13 -0300

    feat(server):basic jest-test in router contactform feat(workspace): husky + lint-staged

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -10,6 +10,8 @@ const app: Application = express();
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use(
 	"/trpc",
 	trpcExpress.createExpressMiddleware({
--- apps/server/src/utils/env.ts
+++ apps/server/src/utils/env.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 const envSchema = z.object({
 	PORT: z.number().default(3333),
+	DATABASE_URL: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
