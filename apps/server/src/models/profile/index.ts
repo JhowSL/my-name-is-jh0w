aaf0d4089baecabab764a63e353e719ffb6c70f4
JhowSL <jon.1lima@hotmail.com>
2024-08-05 21:34:38 -0300

    Revert "feat(server): new router and schema for profile skills"
    This reverts commit 9cfddc6291e669cfbfd4a375f2fde5cdeaf420de.

--- apps/server/src/models/profile/index.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { skillSchema } from "../skills";
-
-export const idSchema = z.object({ id: z.string() });
-
-export const profileSchema = z.object({
-	skills: z.array(skillSchema),
-});
--- apps/server/src/models/skills/index.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const skillSchema = z.object({
-	name: z.string(),
-	level: z.string(),
-});
