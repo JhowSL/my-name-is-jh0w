c7531c8d28c4fed71e9c86cc50a12ee7aee44577
JhowSL <jon.1lima@hotmail.com>
2024-08-08 23:36:56 -0300

    style: applying formatting lint

--- apps/server/src/controllers/certificate/index.ts
+++ apps/server/src/controllers/certificate/index.ts
@@ -23,6 +23,7 @@ export const certificateRouter = connectionPrisma.router({
 						certificates: true,
 					},
 				});
+
 				if (!updatedProfile) {
 					throw new Error("Erro ao adicionar certificados ao perfil");
 				}
@@ -31,15 +32,18 @@ export const certificateRouter = connectionPrisma.router({
 				return errorResponse(error as Error);
 			}
 		}),
+
 	updateCertificate: connectionPrisma.procedure
 		.input(certificateUpdateSchema)
 		.mutation(async ({ input, ctx }) => {
 			try {
 				const { id, title, issuer, date } = input;
+
 				const updatedCertificate = await ctx.prisma.certificate.update({
 					where: { id },
 					data: { title, issuer, date },
 				});
+
 				if (!updatedCertificate) {
 					throw new Error("Erro ao atualizar certificado");
 				}
@@ -54,9 +58,11 @@ export const certificateRouter = connectionPrisma.router({
 		.mutation(async ({ input, ctx }) => {
 			try {
 				const { id } = input;
+
 				const deletedCertificate = await ctx.prisma.certificate.delete({
 					where: { id },
 				});
+
 				if (!deletedCertificate) {
 					throw new Error("Erro ao deletar certificado");
 				}
