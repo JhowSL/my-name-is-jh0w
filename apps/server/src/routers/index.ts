b314637d45964da05aa12de7d9727bc98966e21f
JhowSL <jon.1lima@hotmail.com>
2024-08-08 23:44:40 -0300

    fix: remove router

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -2,7 +2,7 @@ import { certificateRouter } from "../controllers/certificate";
 import { contactRouter } from "../controllers/contact-form";
 import { profileRouter } from "../controllers/profile";
 import { projectRouter } from "../controllers/project";
-import { projectSkillRouter } from "../controllers/project-skill";
+
 import { skillRouter } from "../controllers/skills";
 import { router } from "../utils/trpc";
 
@@ -11,7 +11,6 @@ export const appRouter = router({
 	profile: profileRouter,
 	skill: skillRouter,
 	certificate: certificateRouter,
-	projectSkill: projectSkillRouter,
 	project: projectRouter,
 });
 
