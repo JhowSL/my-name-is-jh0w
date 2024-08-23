da8495acd7cb64857ad1fc7643afbf08953a9322
JhowSL <jon.1lima@hotmail.com>
2024-08-22 21:38:52 -0300

    refactor: update package.json lint script to use "biome check ." instead of "next lint"

--- apps/web/src/components/footer/index.tsx
+++ apps/web/src/components/footer/index.tsx
@@ -5,12 +5,7 @@ import { ContainerFooter } from "../containter";
 export function Footer() {
   return (
     <ContainerFooter>
-      <nav
-        className=" container flex flex-wrap py-4 px-2 
-      "
-      >
-        NavFooter
-      </nav>
+      <nav className=" container flex flex-wrap py-4 px-2">NavFooter</nav>
     </ContainerFooter>
   );
 }
