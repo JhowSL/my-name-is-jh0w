e24ac67562be16425ae1170978fb4a102620d65c
JhowSL <jon.1lima@hotmail.com>
2024-09-03 02:55:01 -0300

    feat(web): project carousel refactor(server):update component imports

---
+++ apps/web/src/components/card/project-card/index.tsx
@@ -0,0 +1,68 @@
+import { Card, CardContent } from "@repo/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@repo/ui/carousel";
+import { GetProjects } from "../../../hooks/use-router";
+import type { ProjectModel } from "../../../models/projects";
+import RepositoryLink from "../../../utils/repository-link";
+import { CardContainer } from "../../containter";
+
+export function ProjectsCard(): JSX.Element {
+  const { getAllProjects } = GetProjects();
+  const { data: projects, isLoading, error } = getAllProjects;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading projects</div>;
+  }
+
+  if (!projects) {
+    return <div>No projects available</div>;
+  }
+  if (!Array.isArray(projects)) {
+    return <div>Unexpected data format</div>;
+  }
+  return (
+    <CardContainer>
+      <div className="grid items-center justify-center">
+        <Carousel className="w-full max-w-xs">
+          <CarouselContent>
+            {projects.map((project: ProjectModel) => (
+              <CarouselItem key={project.id}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6 flex-col">
+                      <span>{project.title}</span>
+                      <span>{project.description}</span>
+                      <span>
+                        <RepositoryLink
+                          url={project.repository}
+                          repoName={project.title}
+                        />
+                      </span>
+                      <div className="flex-col">
+                        {project.technologies.map((tech) => (
+                          <span key={tech.id} className="flex">
+                            {tech.skill.name}
+                          </span>
+                        ))}
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </CardContainer>
+  );
+}
---
+++ apps/web/src/hooks/use-router/index.ts
@@ -0,0 +1,7 @@
+import { trpc } from "../../lib/trpc";
+
+export function GetProjects() {
+  return {
+    getAllProjects: trpc.project.getAllProjects.useQuery(),
+  };
+}
